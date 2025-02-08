20f59723e68b86e0774d2f0bb3bb984d74fec12a
MoushufAlam <rishu78698@gmail.com>
2025-02-08 14:53:32 +0530

    Project Completed

---
+++ Base/src/Components/index.js
@@ -0,0 +1,5 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+//we create a seperate index,js file to import all the files here then export onto the main file so that it doesnt look cluttery!!!
--- Base/src/hooks/useCurrencyInfo.js
+++ Base/src/hooks/useCurrencyInfo.js
@@ -4,10 +4,11 @@ function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
       .then((response) => response.json())
-      .then((response) => setData(response[currency]));
+      .then((response) => setData(response[currency]))
+      .catch((error)=> console.log("Error fetching data : ", error));
   }, [currency]); 
 
   return data;
